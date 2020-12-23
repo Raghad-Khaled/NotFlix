e278fb241de4f6e254fb9245866e4d02d7598fde
Raghad-Khaled <raghoda1234692000@gmail.com>
2020-12-23 23:53:53 +0200

    Fixed

--- Web/SignIn/sign_in_query.php
+++ Web/SignIn/sign_in_query.php
@@ -8,6 +8,8 @@ if(isset($_POST['signin']))
 $email=$_POST['email'];
 $password=$_POST['password'];
 
+//echo $email;
+//echo  $password;
 $user_sign_in =new user;
 
 $user_sign_in->User_Sign_In($email,$password);
