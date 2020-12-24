0034b89b2e6134c232aa1fabd8ae800eeaa32218
Donia Esawi <donya.esawi@gmail.com>
2020-12-24 12:54:28 +0200

    Edit_homePage

---
+++ Web/Home_movies/assets/js/navBar-1-2.js
@@ -0,0 +1,5 @@
+function toggleNav() {
+    document.getElementById("sidebar-nav").classList.toggle('active');
+    document.getElementById("nav-toggle").classList.toggle('active');
+    document.getElementById("dev-report").classList.toggle('active')
+}
