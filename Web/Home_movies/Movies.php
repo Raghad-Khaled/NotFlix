0bd9e266dbff532b2fb2db5e019b713f2e481d01
Raghad-Khaled <raghoda1234692000@gmail.com>
2021-01-12 12:43:26 +0200

    adv in home

--- Web/Home_movies/Movies.php
+++ Web/Home_movies/Movies.php
@@ -446,15 +446,15 @@ $all_ads=$adver->get_all();
                 <!-- Advertisement Card-->
                 <?php  
                 $counter=0;
-                while($row = mysqli_fetch_array($all_ads)  && $counter<8)  { ?>
-               
+                while($row = mysqli_fetch_array($all_ads)  )  if($counter<8) {{ ?>
+              
                 <div class="card" style="margin-top: 60px;">
-                    <div class="card-body" style="height: 100%;width: 100%;"><img src="/Addvertisements/<?=$row['PICTURE']?>" style="width: 100%;"></div>
+                    <div class="card-body" style="height: 100%;width: 100%;"><img src="../AddAdvertisement/Addvertisements//<?=$row['PICTURE']?>" style="width: 100%;"></div>
                 </div>
                
                         <?php 
                     $counter++;
-                    }?>
+                    }}?>
 
                  <!---------------------->
 
