aae84565a324cd7731fa303e44d1157668210989
Donia Esawi <donya.esawi@gmail.com>
2021-01-12 12:54:42 +0200

    changed titles of pages

--- Web/AddFilm/AddFilm.php
+++ Web/AddFilm/AddFilm.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>AddFilm</title>
+  <title>Add a new movie</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Langar&display=swap" rel="stylesheet">
@@ -54,7 +54,7 @@
           <input type="text" class="form-control html-duration-picker" name="duration" required id="Duration" data-hide-seconds>
         </div>
         <div class="col-12 col-lg-4 form-group">
-          <label class="form-label" for="Buget">Buget </label>
+          <label class="form-label" for="Buget">Budget </label>
           <input type="number" class="form-control" id="Buget" required name="buget">
         </div>
         <div class="col-12 col-lg-4 form-group">
--- Web/EditSeries/EditSeries.php
+++ Web/EditSeries/EditSeries.php
@@ -20,7 +20,7 @@ $prize = mysqli_fetch_assoc($reselt5);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EditFilm</title>
+    <title>Edit Series Info</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Langar&display=swap" rel="stylesheet">
@@ -61,7 +61,7 @@ $prize = mysqli_fetch_assoc($reselt5);
 
             <div class="row ">
                 <div class="col-12 col-lg-4 form-group">
-                    <label class="form-label" for="Duration">Duration of the Film </label>
+                    <label class="form-label" for="Duration">Duration of the Series </label>
                     <input type="number" class="form-control" name="duration" required id="Duration" data-hide-seconds value=<?= $data['DURATION_MIN'] ?>>
                 </div>
                 <div class="col-12 col-lg-4 form-group">
